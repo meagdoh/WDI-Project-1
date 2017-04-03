99930907e14d7f5f500ba5488aa0654834862422
meagdoh <meagdoherty@gmail.com>
2017-04-03 15:39:41 -0400

    Add button, answer input and Q&A vars

---
+++ script.js
@@ -0,0 +1,9 @@
+//On load, .contentBox should contain question #1
+
+// When user enters Answer and hits submit, ask if
+//answer matches stored Answer
+
+// An object containing questions and answers
+
+var questionBank = [Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q9,Q10];
+var answerBank = [A1,A2,A3,A4,A5,A6,A7,A8,A9,A10];
