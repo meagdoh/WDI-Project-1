bc9ae363532f98ae971a5087b82f48927998516e
meagdoh <meagdoherty@gmail.com>
2017-04-04 14:25:27 -0400

    Transform Q and A variables to an Object

--- script.js
+++ script.js
@@ -5,8 +5,11 @@
 
 // An array containing questions and answers
 //
-var questionBank = ["Q1","Q2","Q3","Q4","Q5","Q6","Q7","Q8","Q9","Q10"];
-var answerBank = ["A1","A2","A3","A4","A5","A6","A7","A8","A9","A10"];
+var questionAndAnswers ={
+  questionBank: ["Q1","Q2","Q3","Q4","Q5","Q6","Q7","Q8","Q9","Q10"],
+  answerBank: ["A1","A2","A3","A4","A5","A6","A7","A8","A9","A10"]
+};
+
 
 $(document).ready()
 
@@ -19,7 +22,7 @@ var nextButton = $("#nextButton");
 var currentIndex = -1;
 
 nextButton.on("click", function(){
-  displayQuestion.html(questionBank[currentIndex + 1]);
+  displayQuestion.html(questionAndAnswers.questionBank[currentIndex + 1]);
   currentIndex++;
   if(currentIndex === 9) {
     currentIndex = -1;
