05eaf28689da1966cb83f5085450170b60f0fd88
meagdoh <meagdoherty@gmail.com>
2017-04-04 09:40:27 -0400

    Add Question Bank loop

--- script.js
+++ script.js
@@ -5,15 +5,32 @@
 
 // An array containing questions and answers
 //
-// var questionBank = [Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q9,Q10];
-// var answerBank = [A1,A2,A3,A4,A5,A6,A7,A8,A9,A10];
+var questionBank = ["Q1","Q2","Q3","Q4","Q5","Q6","Q7","Q8","Q9","Q10"];
+var answerBank = ["A1","A2","A3","A4","A5","A6","A7","A8","A9","A10"];
 
 $(document).ready()
 
-var button = $("#submitInput");
+var submitButton = $("#submitInput");
+var displayQuestion = $("#card");
+var nextButton = $("#nextButton");
 
-function checkAnswer(){
-  console.log("I was clicked!");
-};
+nextButton.on("click", function(){
+  console.log("I was clicked!")
+})
 
-button.on("click", checkAnswer);
+submitButton.on("click", function(){
+  console.log("I was clicked!")
+  var userInput = $("#answerInput").val()
+  console.log(userInput)
+  displayQuestion.html(questionBank[0])
+});
+
+questionBank.forEach(function(currentQuestion) {
+  console.log(currentQuestion)
+});
+
+// //flipping card
+//   $("#card").flip({
+//     axis: 'x',
+//     trigger: 'hover'
+//   });
