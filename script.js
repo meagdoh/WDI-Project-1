bf62e8ad406661f3368e132b9135af3652fec2ba
meagdoh <meagdoherty@gmail.com>
2017-04-05 15:25:31 -0400

    Hide display answer

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 
 $(document).ready()
 
-// Transform Q&A variables into a series of objects
-    var questionAndAnswers =[
-       {question: "question 1", answer: "answer 1"},
-       {question: "question 2", answer: "answer 2"},
-       {question: "question 3", answer: "answer 3"},
-       {question: "question 4", answer: "answer 4"},
-       {question: "question 5", answer: "answer 5"},
-       {question: "question 6", answer: "answer 6"},
-       {question: "question 7", answer: "answer 7"},
-       {question: "question 8", answer: "answer 8"},
-       {question: "question 9", answer: "answer 9"},
-       {question: "question 10", answer: "answer 10"},
-    ];
-
-var results = [];
+// Variables
+var questionAndAnswers =[
+   {question: "question 1", answer: "answer 1"},
+   {question: "question 2", answer: "answer 2"},
+   {question: "question 3", answer: "answer 3"},
+   {question: "question 4", answer: "answer 4"},
+   {question: "question 5", answer: "answer 5"},
+   {question: "question 6", answer: "answer 6"},
+   {question: "question 7", answer: "answer 7"},
+   {question: "question 8", answer: "answer 8"},
+   {question: "question 9", answer: "answer 9"},
+   {question: "question 10", answer: "answer 10"},
+];
 var showAnswerButton = $("#showAnswerButton");
 var submitButton = $("#submitInput");
 var displayQuestion = $("#questionCard");
 var displayAnswer = $("#answerCard");
 var nextButton = $("#nextButton");
 var answerResponse = $("#answerResponse");
+var userScore = $("asideBox");
 var webPage = $(document);
-var currentQuestionIndex = -1;
-var currentAnswerIndex = -1;
+var currentQuestionIndex = 0;
+var currentAnswerIndex = 0;
 
-// Looping through the list of questions and answers
-  nextButton.on("click", function(){
+//On load, .contentBox contains question #1
+displayQuestion.html(questionAndAnswers[0].question);
+displayAnswer.html(questionAndAnswers[0].answer);
+
+// Click through the list of questions and answers
+nextButton.on("click", setCurrentQuestion);
+
+// Set current question and answer
+function setCurrentQuestion() {
   displayQuestion.html(questionAndAnswers[currentQuestionIndex + 1].question);
   currentQuestionIndex++;
   if(currentQuestionIndex === 9) {
@@ -38,25 +44,34 @@ var currentAnswerIndex = -1;
   if(currentAnswerIndex === 9) {
     currentAnswerIndex = -1;
   }
-});
+}
 
-// // Checking user input against answers
+
+// Show answer card. *Needs to default to hidden.
+ showAnswerButton.click(function() {
+    displayAnswer.toggle( "slow" );
+  });
+
+// Checking user input against answers. Store results in results array.
 submitButton.on("click", function(){
   var userInput = $("#answerInput").val()
   if (userInput === questionAndAnswers[currentAnswerIndex].answer) {
     answerResponse.html("Correct! Click 'Next'");
-    results.push(true);
     }
   else {
     answerResponse.html("Try again.");
-    results.push(false);
     }
   });
 
-// Scoring
-  // If answerResponse = correct, remove from array.
-  // If answerResponse = incorrect, keep in array.
-  // When array = 0, display 'Complete!'
+//Check results for
+
+  //
+  // var trackScore = function(){
+  //   var correctAnswer = [currentQuestionIndex]
+  //   // If answerResponse = true, remove from questionAndAnswers.
+  //   // If answerResponse = false, do nothing.
+  //   // When array = 0, display 'Complete!'
+  // };
 
 // webPage.on('keypress', function (event) {
 //           if (event.keyCode == 39) {
@@ -66,15 +81,6 @@ submitButton.on("click", function(){
 //               alert("Not Enter key pressed");
 //           }
 //       });
-
-      // showAnswer.click(function() {
-      //   displayAnswer.toggle( "slow", function() {
-      //     // Animation complete.
-      //   });
-      // });
-
-
-//On load, .contentBox should contain question #1
 
 
 //Card display
@@ -86,8 +92,3 @@ submitButton.on("click", function(){
 // After you click nextButton, clear #answerResponse
 
 // toggle jQuery.hide (on click Answer button)
-
-
-  // showAnswerButton.on("click",(function(){
-  //       displayAnswer.slideToggle('slow');
-  //   }));
